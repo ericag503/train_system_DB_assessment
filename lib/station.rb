3b95a75e2d509dd117cdfd9508506bfb924df26b
Erica Gordon <e.gordon503@gmail.com>
2014-08-19 14:42:18 -0700

    Creates functionality to save a new station to the database.

--- lib/station.rb
+++ lib/station.rb
@@ -2,8 +2,30 @@ class Station
 	attr_reader :name, :id
 
 	def initialize(attributes)
-		@name = attributes['name']
-		@id = attributes['id'].to_i
+		@name = attributes[:name]
+		@id = attributes[:id]
 	end 
+
+	def save
+		results = DB.exec("INSERT INTO stations (name) VALUES ('#{name}') RETURNING id;")
+		@id = results.first['id'].to_i
+	end 
+
+	def self.all
+		results = DB.exec("SELECT * FROM stations;")
+		stations = []
+		results.each do |result|
+			name = result['name']
+			new_station = Station.new(:name => name)
+			stations << new_station
+		end
+		stations
+	end
+
+	def ==(another_station)
+		self.name == another_station.name 
+	end
+
+
 end
 
